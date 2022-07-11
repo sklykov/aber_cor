1d968b0f8a304cfa84e05e159a25c3af8b3885a6
ssklykov <sergej.klykow@gmail.com>
2022-07-11 15:11:53 +0200

    Removed the folder with the project for PCO camera ctrl

--- README.md
+++ README.md
@@ -2,9 +2,4 @@
 Collection of Python scripts for reconstruction and estimation of optical aberrations. 
 
 - A few script there focus on calculation and representation of Zernike polynomials sums and subsequent
-superposed phase fronts. Another - on controlling specific sCMOS camera, performing wavefront reconstruction using
-open algorithms.  
-- All scripts have been developed with common dependencies for numerical calculations: scipy(numpy),
-matplotlib, scikit-image (skimage), pyqt5, pyqtgraph and, additionally, device controlling libraries.
-I don't see any necessity to specify it now in any "requirements" file for installation from a scratch. In the case of 
-any interest, please, refer to import sections of the scripts.
+superposed phase fronts. See the requirements file for the dependency list.
--- wfs/setup.py
+++ wfs/setup.py
@@ -6,13 +6,12 @@ Specify all dependecies for installing the wavefront reconstruction package.
 @license: GPLv3 (check https://www.gnu.org/licenses/ )
 """
 from setuptools import setup
-# from setuptools import find_packages
 
 with open("README.md", "r", encoding="utf-8") as readme_info:
     long_description = readme_info.read()
 
 setup(name="wavefront_reconstruction",
-      version="0.0.3.dev1", license="GPLv3",
+      version="0.0.3", license="GPLv3",
       license_files=['LICENSE'],
       url="https://github.com/ssklykov/aber_cor/tree/main/wfs",
       download_url="https://github.com/ssklykov/aber_cor/tree/main/wfs/for_wheel",
@@ -22,8 +21,5 @@ setup(name="wavefront_reconstruction",
       package_data={"wfs_reconstruction": ['calibrations/*', 'pics/*', 'README.md']},
       # data_files=[('.', ['README.md'])],  # adding some info into data folder only
       include_package_data=True,
-      # packages=find_packages(),
-      # py_modules=["gui_wfs_reconstruction", "calc_zernikes_sh_wfs",
-      #             "reconstruction_wfs_functions", "zernike_pol_calc"],
       description="GUI and functions for reconstruction of wavefronts acquired by a Shack-Hartmann sensor",
       install_requires=['matplotlib', 'numpy', 'scikit-image', 'scipy'])
